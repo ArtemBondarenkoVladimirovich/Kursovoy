0347e7fff3147e3f14d35257a26b1dc40a9f3978
vladimir bondarenko <vovan762000@yahoo.com>
2019-04-14 19:52:50 +0300

    Добавьте файлы проекта.

---
+++ Kursovoy/bus.h
@@ -0,0 +1,80 @@
+#pragma once
+#include <string>
+#include <set>
+
+class Bus
+{
+private:
+	int number;
+	std::string type;
+	std::string destination;
+	std::string startTime;
+	std::string endTime;
+public:
+	Bus()
+	{
+		
+	}
+	Bus(int number, std::string type, std::string destination, std::string startTime, std::string endTime)
+	{
+		this->number = number;
+		this->type = type;
+		this->destination = destination;
+		this->startTime = startTime;
+		this->endTime = endTime;
+	}
+	~Bus()
+	{
+
+	}
+
+	void setNumber(int number)
+	{
+		this->number = number;
+	}
+
+	void setType(std::string type)
+	{
+		this->type = type;
+	}
+
+	void setDestination(std::string destination)
+	{
+		this->destination = destination;
+	}
+
+	void setStartTime(std::string startTime)
+	{
+		this->startTime = startTime;
+	}
+
+	void setEndTime(std::string endTime)
+	{
+		this->endTime = endTime;
+	}
+
+	int getNumber()
+	{
+		return number;
+	}
+
+	std::string getType()
+	{
+		return type;
+	}
+
+	std::string getDestination()
+	{
+		return destination;
+	}
+
+	std::string getStartTime()
+	{
+		return startTime;
+	}
+
+	std::string getEndTime()
+	{
+		return endTime;
+	}
+};
